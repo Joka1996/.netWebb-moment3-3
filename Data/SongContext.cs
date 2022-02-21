b63f65fb559d3572ea51cb3fa17dbf58a0089ab1
Joel K <duveled@gmail.com>
2022-02-21 15:42:33 +0100

    Add project files.

---
+++ Data/SongContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using moment3_3.Models;
+
+namespace moment3_3.Data
+{   //ärva
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+
+        }
+        //skapa tabell med dbset
+        public DbSet<Songs> Songs { get; set; }
+    }
+}
---
+++ Models/Songs.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moment3_3.Models
+{
+    public class Songs
+    {
+        //properties
+        public int Id { get; set; }
+        [Required]
+        public string? Artist { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public int Length { get; set; }
+        [Required]
+        public string? Category { get; set; }
+        
+    }
+
+}
