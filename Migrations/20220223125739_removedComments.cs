943616af94d8b3b8f4c96dd77e56353aa4c89156
Joel K <duveled@gmail.com>
2022-02-23 15:00:03 +0100

    migrations

---
+++ Migrations/20220223125739_removedComments.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace moment3_3.Migrations
+{
+    public partial class removedComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
